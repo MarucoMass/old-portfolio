0b627abc531e054e02c4ae54725bc153ec0ac0c1
MarucoMass <mariomass23@gmail.com>
2022-12-13 04:34:44 -0300

    deleted email.js, changed styles of the titles

--- js/contacto.js
+++ js/contacto.js
@@ -2,29 +2,13 @@ const btn = document.getElementById('button');
 
 document.getElementById('form')
  .addEventListener('submit', function(event) {
+
    event.preventDefault();
-
-   btn.value = 'Enviando...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_y54wuyi';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Enviar';
-      Swal.fire({
-        title: 'Enviado',
-        icon: 'success',
-        text: 'Gracias por contactarte conmigo!',
-      });
-    }, (err) => {
-      btn.value = 'Enviar';
-      Swal.fire({
-        title: 'Parece que hubo un problema',
-        icon: 'error',
-        text: JSON.stringify(err),
-      });
-    });
+   Swal.fire({
+     title: 'Enviado',
+     icon: 'success',
+     text: 'Gracias por contactarte conmigo!',
+   });
 
     document.getElementById('form').reset();
     
